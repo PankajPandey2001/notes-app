7568f894751c85fce81baa003447bb38c97d23bd
Pankaj Pandey <pankajpnd2001@gmail.com>
2024-11-18 22:50:47 +0530

    new note adding feature add

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ const App = () => {  // function name should be in caps as well as name of js fi
 
    const addNote = (notetext) => {
        
+
+    // now we have text of note , par date nhi hai toh date bana do fir dal do 
+     
+    const date = new Date() ; 
+
         const note = {
            text : notetext ,
-           
-        }
+           date : date.toLocaleDateString()
+        } ; 
+
+        const newNotes = [...notes , note] ; // now see humne is line mein kya kiya  , jo phle notes ki list thi (notes)
+        // usko mangaya , by ading three .  then comma lagake current note jod do 
+
+        // ab setNotes se notes ko update kar do 
+
+        setNotes(newNotes) ; 
 
    }
 
